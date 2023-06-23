99bb3832f0167b6c32a2c1cc98f6184d0f828c26
Elizaveta9988 <109989600+Elizaveta9988@users.noreply.github.com>
2023-06-23 23:05:50 +0300

    first commit

---
+++ src/ru/gb/jcore/task4/AmountException.java
@@ -0,0 +1,9 @@
+package ru.gb.jcore.task4;
+
+public class AmountException {
+    public class AmountException extends Exception {
+        public AmountException(String message) {
+            super(message);
+        }
+    }
+}
---
+++ src/ru/gb/jcore/task4/Product.java
@@ -0,0 +1,87 @@
+package ru.gb.jcore.task4;
+
+public class Product (String name, double price, String category) {
+    setName(name);
+    setPrice(price);
+    setCategory(category);
+    this.discountSize = DiscountSize.NO_DISCOUNT;
+}
+    public static void assignRandomDiscount() {
+        DiscountSize[] discounts = DiscountSize.values();
+        Random random = new Random();
+        int index = random.nextInt(discounts.length);
+        DiscountSize randomDiscount = discounts[index];
+
+        Product product = new Product("Sample Product", 100.0, Category.NORMAL);  // Создаем экземпляр класса Product
+        try {
+            product.setDiscountSize(randomDiscount);  // Вызываем метод setDiscountSize() на экземпляре класса Product
+        } catch (TooMuchSaleException e) {
+            System.out.println("TooMuchSaleException: " + e.getMessage());
+        }
+    }
+
+
+    // Геттеры и сеттеры
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Product name cannot be null or empty.");
+        }
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("Product price cannot be negative.");
+        }
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        if (category == null) {
+            throw new IllegalArgumentException("Category cannot be null.");
+        }
+        this.category = category;
+    }
+
+    public DiscountSize getDiscountSize() {
+        return discountSize;
+    }
+
+    public void setDiscountSize(DiscountSize discountSize) throws TooMuchSaleException {
+        if (discountSize == null) {
+            throw new IllegalArgumentException("Discount size cannot be null.");
+        }
+        if (this.category == Category.PREMIUM && discountSize.getValue() > 0.15) {
+            throw new TooMuchSaleException("Discount greater than 15% is not allowed for premium products.");
+        }
+        this.discountSize = discountSize;
+    }
+
+
+
+
+
+
+    @Override
+    public String toString() {
+        return
+                "Название " + name + '\n' +
+                        " Цена " + price + '\n' +
+                        " Категория " + category + '\n' +
+                        " Скидка " + discountSize + '\n';
+    }
+}
+}
